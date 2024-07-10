2a347780d5e82ddd1516c8a367ed46c3b169f7b6
bruch-alex <alex.bruch@tutanota.com>
2024-07-10 11:55:59 +0200

    add method, that increments value

--- Main.java
+++ Main.java
@@ -1,3 +1,17 @@
-public class Main{
+import java.util.Scanner;
 
+public class Main{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter an integer value");
+        int value = s.nextInt();
+        System.out.println("Before method: " + value);
+        increment(value);
+        System.out.println("After method: " + value);
+    }
+
+    public static void increment(int value){
+        value++;
+        System.out.println("Inside method: " + value);
+    }
 }
